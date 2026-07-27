a6b8e34f66c1ab779127b6c7cd73926ef527bfe0
Pallavi567107 <pallaviyadav567107@gmail.com>
2026-07-27 19:10:07 +0530

    Update greeetuser.py

--- pythonprogramming/input_output_statements/greeetuser.py
+++ pythonprogramming/input_output_statements/greeetuser.py
@@ -1,7 +1,10 @@
 #I/O statements 
 # task :1
 # Name:Pallavi Dhuli
-print("program 1")
+
 name = input("Enter name: ")
 age = int(input("Enter age: "))
 print(f"Hello {name}, you will turn {age + 1} next year.")
+#Enter name: pallavi
+#Enter age: 19
+#Hello pallavi, you will turn 20 next year.
