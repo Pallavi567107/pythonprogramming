359d991f2b94b72582dc9cc204470cd17c267e61
Pallavi567107 <pallaviyadav567107@gmail.com>
2026-07-27 18:49:43 +0530

    Enhance variable print statements with type info
    Added type information for variables in print statements.

--- pythonprogramming/variables/variables_assignment.py
+++ pythonprogramming/variables/variables_assignment.py
@@ -9,3 +9,7 @@ print(name," type:", type(name))
 print(height," type:",type(height))
 print(age," type:",type(age))
 print(is_student," type:",type(is_student))
+#pallavi  type: <class 'str'>
+#5.9  type: <class 'float'>
+#19  type: <class 'int'>
+#True  type: <class 'bool'>
