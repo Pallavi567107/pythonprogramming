8f0eae2116987906edd4d30eef8d42120e6907a6
Pallavi567107 <pallaviyadav567107@gmail.com>
2026-07-27 18:59:24 +0530

    Add total keywords comment to totalkeywords.py
    Added a comment with the total number of keywords in Python.

--- pythonprogramming/keywords/totalkeywords.py
+++ pythonprogramming/keywords/totalkeywords.py
@@ -6,3 +6,5 @@ l=len(keyword.kwlist)
 print(l)
 for keywords in keyword.kwlist:
    print(keywords)
+  # 35
+#False None True and as assert async await break class continue def del elif else except finally for from global if import in is lambda nonlocal not or pass raise return try while with yield 
