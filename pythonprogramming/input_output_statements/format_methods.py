3594f5c95968850c45762d90ddf344e2171d5c9b
Pallavi567107 <pallaviyadav567107@gmail.com>
2026-07-27 19:11:03 +0530

    Update format_methods.py

--- pythonprogramming/input_output_statements/format_methods.py
+++ pythonprogramming/input_output_statements/format_methods.py
@@ -5,3 +5,6 @@ name = "Pallavi"; marks = 85
 print(name, marks) # comma-separated
 print("Name: {} Marks: {}".format(name, marks)) # str.format
 print(f"Name: {name} Marks: {marks}") # f-string
+#Pallavi 85
+#Name: Pallavi Marks: 85
+#Name: Pallavi Marks: 85
