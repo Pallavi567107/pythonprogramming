c1da7e64865d5463a0e852570e1dec4e75bae9cd
Pallavi567107 <pallaviyadav567107@gmail.com>
2026-08-03 12:17:41 +0530

    Add files via upload

---
+++ operators/login_credentials.py
@@ -0,0 +1,16 @@
+name="pallavi"
+password="567"
+user=input("enter username")
+userid=input("enter useri id")
+
+if((user==name)and(userid==password)):
+  login_status=True
+else :
+    login_status=False
+if(login_status):
+    print("login successfull")
+else:
+    print("invalid login credentials")
+ 
+  
+ 
